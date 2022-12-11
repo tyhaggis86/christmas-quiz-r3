fae3cec81dcf687935d3e310232d8d84ec4e1502
tyhaggis86 <113810488+tyhaggis86@users.noreply.github.com>
2022-12-11 15:10:48 +1000

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,167 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which plant often given as a gift at Christmas has bright red and green leaves and is sometimes known as the Christmas flower?",
+    answer: "Poinsettia",
+    options: [
+      "Holly",
+      "Douglas Fir",
+      "Mistletoe",
+      "Poinsettia"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many tips does a snowflake have?",
+    answer: "6",
+    options: [
+      "4",
+      "6",
+      "8",
+      "10"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which Christmas product did Sir Henry Cole produce in 1843?",
+    answer: "Christmas Cards",
+    options: [
+      "Christmas Cards",
+      "Holly Wreath",
+      "Christmas Tree",
+      "Christmas Crackers"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who is the patron saint celebrated on the 26th December?",
+    answer: "Saint Stephen",
+    options: [
+      "Saint Patrick",
+      "Saint George",
+      "Saint Stephen",
+      "Saint Anthony"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How long does it take for a tree to reach the average, ideal Christmas tree height?",
+    answer: "15 years",
+    options: [
+      "15 years",
+      "10 years",
+      "6 months",
+      "12 months"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Name the animatronic cassette-playing bear toy that every kid wanted for Christmas in the mid-80s?",
+    answer: "Teddy Ruxpin",
+    options: [
+      "Furby",
+      "Teddy Ruxpin",
+      "Peek a Boo Teddy Bear",
+      "Tickle Me Elmo"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which U.S. state was the first to declare Christmas an official holiday?",
+    answer: "Alabama",
+    options: [
+      "New York",
+      "California",
+      "Texas",
+      "Alabama"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What is the most recorded Christmas song of all time?",
+    answer: "Silent Night",
+    options: [
+      "Jingle Bells",
+      "Silent Night",
+      "Deck the Halls",
+      "Away in a Manger"
+    ]
+  },
+  {
+    numb: 9,
+    question: "In The Muppet Christmas Carol, who played Scrooge?",
+    answer: "Michael Caine",
+    options: [
+      "Kermit the Frog",
+      "Fozzie Bear",
+      "Michael Caine",
+      "Jim Carey"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Who invented electric Christmas lights?",
+    answer: "Thomas Edison",
+    options: [
+      "Thomas Edison",
+      "Nikola Tesla",
+      "Benjamin Franklin",
+      "Alexander Graham Bell"
+    ]
+  },
+  {
+    numb: 11,
+    question: "How many gifts in total were given in “The Twelve Days of Christmas” song?",
+    answer: "364",
+    options: [
+      "12",
+      "144",
+      "253",
+      "364"
+    ]
+  },
+  {
+    numb: 12,
+    question: "What spy hides around the house, reporting back to Santa on who has been naughty and nice?",
+    answer: "Elf on the Shelf",
+    options: [
+      "Robin Redbreast",
+      "Frosty the Snowman",
+      "Elf on the Shelf",
+      "Jack Skellington"
+    ]
+  },
+  {
+    numb: 13,
+    question: "In which century was the term Xmas coined?",
+    answer: "16th",
+    options: [
+      "14th",
+      "16th",
+      "18th",
+      "20th"
+    ]
+  },
+  {
+    numb: 14,
+    question: "What was the highest grossing Christmas movie of all time?",
+    answer: "The Grinch",
+    options: [
+      "The Grinch",
+      "Home Alone",
+      "Miracle on 34th Street",
+      "It's a Wonderful Life"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Is Die Hard a Christmas movie?",
+    answer: "Yes",
+    options: [
+      "Yes",
+      "Yes",
+      "Yes",
+      "Yes"
+    ]
+  },
+];
